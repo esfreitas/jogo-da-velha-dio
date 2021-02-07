627e62478f4857ef304975e745214e2d5b1928ad
Elton Freitas <eltons.freitas01@gmail.com>
2021-02-06 22:12:59 -0300

    Edição das funções escolherQuadrado() e mudarJogador()

--- js/js.js
+++ js/js.js
@@ -1,5 +1,26 @@
+var jogador = null;
+
+var jogadorSelecionado = document.getElementById('jogador-selecionado');
+
+mudarJogador('X');
+
 function escolherQuadrado(id){
-    console.log(id);
+
+    var quadrado = document.getElementById(id);
+
+    quadrado.innerHTML = jogador;
+    quadrado.style.color = '#000';
+
+    if (jogador === 'X'){
+        mudarJogador('O')
+    }else{
+        mudarJogador('X')
+    }
+}
+
+function mudarJogador(valor){
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
 }
 
 
